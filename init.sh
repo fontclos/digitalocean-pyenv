3b40edfe71ecc0add6ced48c26a40317ec7704a0
Francesc Font-Clos <francesc.font@gmail.com>
2019-01-27 20:08:52 +0100

    install python versions

--- init.sh
+++ init.sh
@@ -13,8 +13,20 @@ xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 # make new user
 # bash create_new_user.sh
 curl https://raw.githubusercontent.com/fontclos/digitalocean-pyenv/master/create_new_user.sh?token=AJ99IZAQns_J-oPIUalmE1w6YA_eboF8ks5cVzlcwA%3D%3D | bash
+
 # switch to new user
 su - fontclos
 
 # install pyenv
 curl https://pyenv.run | bash
+
+# add pyenv stuff to bashrc
+echo "export PATH=\"/home/fontclos/.pyenv/bin:\$PATH\"
+eval \"\$(pyenv init -)\"
+eval \"\$(pyenv virtualenv-init -)\"" >> /home/fontclos/.bashrc
+
+# install a few python versions
+pyenv install 3.7.0
+pyenv install 3.6.0
+pyenv install 3.5.0
+pyenv install 3.4.0
