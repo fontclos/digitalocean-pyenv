46e490bc630d134aa02cf7a0fd525e2c5c35d316
Francesc Font-Clos <francesc.font@gmail.com>
2019-01-27 19:57:41 +0100

    make reference to create_new_user via rawgithub

--- init.sh
+++ init.sh
@@ -11,8 +11,8 @@ libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
 # make new user
-bash create_new_user.sh
-
+# bash create_new_user.sh
+curl https://raw.githubusercontent.com/fontclos/digitalocean-pyenv/master/create_new_user.sh?token=AJ99IZAQns_J-oPIUalmE1w6YA_eboF8ks5cVzlcwA%3D%3D | bash
 # switch to new user
 su - fontclos
 
