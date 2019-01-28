3eb69b362684c8f827a97e436a118938d9b65320
Francesc Font Clos <francesc.font@gmail.com>
2019-01-28 08:58:31 +0100

    sourceing bashrc

--- second_run_as_user.sh
+++ second_run_as_user.sh
@@ -8,20 +8,23 @@ echo "export PATH=\"/home/fontclos/.pyenv/bin:\$PATH\"
 eval \"\$(pyenv init -)\"
 eval \"\$(pyenv virtualenv-init -)\"" >> /home/fontclos/.bashrc
 
-source /home/fontclos/.bashrc
+# like sourceing bashrc but skipping the first 10 lines
+# to skip the non-interactive shell description
+# https://askubuntu.com/questions/64387/cannot-successfully-source-bashrc-from-a-shell-script
+eval "$(cat ~/.bashrc | tail -n +10)"
 
 # install a few python versions
 pyenv install 3.7.0
-#pyenv install 3.6.0
-#pyenv install 3.5.0
-#pyenv install 3.4.0
-#pyenv install 3.3.0
-#pyenv install 3.2
-#pyenv install 3.1
-#pyenv install 2.7
-#pyenv install 2.6.6
-#pyenv install 2.5
-#pyenv install 2.4
-#pyenv install 2.3.7
-#pyenv install 2.2.3
+pyenv install 3.6.0
+pyenv install 3.5.0
+pyenv install 3.4.0
+pyenv install 3.3.0
+pyenv install 3.2
+pyenv install 3.1
+pyenv install 2.7
+pyenv install 2.6.6
+pyenv install 2.5
+pyenv install 2.4
+pyenv install 2.3.7
+pyenv install 2.2.3
 pyenv install 2.1.3
